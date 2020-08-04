9aa0eaa5de696611b5777527a28637613df8d667
karun <karunahuja1998@gmail.com>
2020-08-04 18:01:19 +0530

    how to fetch data of form using servlet

---
+++ WebApplication1/src/java/com/practice/RegisterServlet.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.practice;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.xml.ws.Response;
+/**
+ *
+ * @author Karun
+ */
+public class RegisterServlet extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        resp.setContentType("text/html");
+          PrintWriter out=resp.getWriter();
+     out.println("<h1>post method of servlet</h1>");
+     String name=req.getParameter("user_name");
+     String password=req.getParameter("user_password");     
+     String email=req.getParameter("user_email");
+  String gender=req.getParameter("user_gender");
+    String course=req.getParameter("user_course");
+    String cond=req.getParameter("condition");
+    
+//    String cond=RequestDispatcher.getParameter("condition");
+    if(cond.equals("checked")){
+        
+        out.println("<h2>Name:"+name+"</h2>");
+        
+        
+        out.println("<h2>email:"+email+"</h2>");
+        out.println("<h2>password:"+password+"</h2>");
+        
+        out.println("<h2>gender:"+gender+"</h2>");
+        
+        out.println("<h2>Course:"+course+"</h2>");
+    }
+    else{
+        out.println("COnditions unaccepeted");
+    }
+    
+//To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
